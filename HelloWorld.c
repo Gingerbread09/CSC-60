6df0b5a2839d7c685647510b50e08813e682414d
GingerBread09 <ashwanipanicker@csus.edu>
2025-01-29 00:35:41 -0800

    Program to introduce self and print a quote.

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+/*------------------*/
+/* Ashwani Panicker */
+/* Lab1 */
+/*------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+printf("\nLab1 \n\n");
+printf("Hello World\n");
+printf("Hi, my name is Ashwani Panicker. \n\n");
+printf("Education is the chief defense of the nation. \n-Burke \n");
+return EXIT_SUCCESS;
+}
