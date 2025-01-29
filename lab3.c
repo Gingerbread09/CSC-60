fb49d0443379d51fc8ab7adb01512e443819f88e
GingerBread09 <ashwanipanicker@csus.edu>
2025-01-29 00:42:54 -0800

    Lab 3: Display age, average, and quotes

---
+++ lab3.c
@@ -0,0 +1,19 @@
+/*------------------*/
+/* Ashwani Panicker */
+/* Lab 3            */
+/*------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+int age = 22;
+double average = 246.13579;
+printf("\nAshwani Panicker. Lab 3.\n");
+printf("\nJane went to the store.\n");
+printf("\nMy friend is %i years old.\n",age);
+printf("\n\"So many books, so little time.\"\n-Frank Zappa.\n");
+printf("\nTheAverage = %5.1lf \n",average);
+printf("\nTheAverage = %7.3lf \n",average);
+printf("\nTheAverage = %.2e \n\n" ,average);
+}
